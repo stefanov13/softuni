80b3f7399df047773e0fc738181fb9a0c5760621
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2022-12-23 12:34:32 +0200

    Delete Python Advanced - Exercises directory

--- Python Advanced - Exercises/Lists as Stacks and Queues/reverse_numbers.py
+++
@@ -1,4 +0,0 @@
-nums_stack = input().split()
-
-for _ in range(len(nums_stack)):
-    print(nums_stack.pop(), end= " ")
--- Python Advanced - Exercises/Tuples and Sets/periodic_table.py
+++
@@ -1,8 +0,0 @@
-count_of_lines = int(input())
-
-unique_elements = set()
-for _ in range(count_of_lines):
-    elements = input().split()
-    element_ = [unique_elements.add(el) for el in elements]
-
-print('\n'.join(unique_elements))
