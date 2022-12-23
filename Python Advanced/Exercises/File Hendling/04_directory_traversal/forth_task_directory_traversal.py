7e99b10a5c331ee5bdcc9e90c917d9f26ba2c596
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2022-12-23 13:03:40 +0200

    Add files via upload

---
+++ Python Advanced/Exercises/File Hendling/04_directory_traversal/forth_task_directory_traversal.py
@@ -0,0 +1,32 @@
+import os
+
+result = {}
+
+
+def get_directory_files(directory):
+    return [x for x in os.listdir(rf"{directory}") if os.path.isfile(rf"{directory}\{x}")]
+
+
+def save_report(data, directory):
+    with open(f"{directory}\\report.txt", "a", encoding="utf-8") as file:
+        file.write(data)
+
+
+def prepare_for_data_for_save(directory):
+    files = get_directory_files(directory)
+    for file in files:
+        extension = file[file.find("."):]
+        result[extension] = result.get(extension, []) + [file]
+    sorting_data_for_save(directory)
+
+
+def sorting_data_for_save(directory):
+    data = ""
+    for extension in sorted(result.keys()):
+        data += f"{extension}\n"
+        for file in sorted(result[extension]):
+            data += f"- - - {file}\n"
+    save_report(data.rstrip(), directory)
+
+
+prepare_for_data_for_save(input())
