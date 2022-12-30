329e13551c2441c946901593534213fc4f6b6a0e
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2022-12-30 11:47:17 +0200

    Add files via upload

---
+++ Python OOP/Python OOP - Exercises/Encapsulation/04_restaurant/project/product.py
@@ -0,0 +1,34 @@
+from project.beverage.beverage import Beverage
+from project.food.soup import Soup
+
+
+class Product:
+    def __init__(self, name: str, price: float):
+        self.__name = name
+        self.__price = price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def price(self):
+        return self.__price
+
+
+product = Product("coffee", 2.5)
+print(product.__class__.__name__)
+print(product.name)
+print(product.price)
+beverage = Beverage("coffee", 2.5, 50)
+print(beverage.__class__.__name__)
+print(beverage.__class__.__bases__[0].__name__)
+print(beverage.name)
+print(beverage.price)
+print(beverage.milliliters)
+soup = Soup("fish soup", 9.90, 230)
+print(soup.__class__.__name__)
+print(soup.__class__.__bases__[0].__name__)
+print(soup.name)
+print(soup.price)
+print(soup.grams)
