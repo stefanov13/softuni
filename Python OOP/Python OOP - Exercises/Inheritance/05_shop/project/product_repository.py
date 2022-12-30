3862115454f54da55d44942ef9639103fba4ca28
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2022-12-30 11:44:01 +0200

    Add files via upload

---
+++ Python OOP/Python OOP - Exercises/Inheritance/05_shop/project/product_repository.py
@@ -0,0 +1,23 @@
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str):
+        for p in self.products:
+            if p.name == product_name:
+                return p
+    
+    def remove(self, product_name: str):
+        product = self.find(product_name)
+
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return '\n'.join([f"{p}: {p.quantity}" for p in self.products])
